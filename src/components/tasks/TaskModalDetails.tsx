1a88f8704f70bd82037cb1f5dc19faffb2c1fe68
EdgarDNavarro <edgardaniel2107np@gmail.com>
2024-12-09 20:34:26 +0100

    start notes

--- src/components/tasks/TaskModalDetails.tsx
+++ src/components/tasks/TaskModalDetails.tsx
@@ -7,6 +7,7 @@ import { toast } from 'react-toastify';
 import { formatDate } from '@/utils/utils';
 import { statusTranslations } from '@/locales/es';
 import { TaskStatus } from '@/types/index';
+import NotesPanel from '../notes/NotesPanel';
 
 export default function TaskModalDetails() {
     const params = useParams()
@@ -85,16 +86,22 @@ export default function TaskModalDetails() {
 
                                     <p className='text-lg text-slate-500 mb-2'>Descripción: {data.description} </p>
 
-                                    <p className='text-2xl text-slate-500 mb-2'>Historial de cambios</p>
+                                    {data.completedBy.length ? (
+                                        <>
 
-                                    <ul className='list-decimal'>
-                                        {data.completedBy.map((activityLog) => (
-                                            <li key={activityLog._id}>
-                                                <span className='font-bold text-slate-600'>{statusTranslations[activityLog.status]}</span>
-                                                {' '} por: {activityLog.user.name}
-                                            </li>
-                                        ))}
-                                    </ul>
+                                            <p className='text-2xl text-slate-500 mb-2'>Historial de cambios</p>
+
+                                            <ul className='list-decimal'>
+                                                {data.completedBy.map((activityLog) => (
+                                                    <li key={activityLog._id}>
+                                                        <span className='font-bold text-slate-600'>{statusTranslations[activityLog.status]}</span>
+                                                        {' '} por: {activityLog.user.name}
+                                                    </li>
+                                                ))}
+                                            </ul>
+
+                                        </>
+                                    ) : null }
 
                                     <div className='my-5 space-y-3'>
                                         <label className='font-bold'>Estado Actual: {statusTranslations[data.status]}</label>
@@ -108,6 +115,9 @@ export default function TaskModalDetails() {
                                             ))}
                                         </select>
                                     </div>
+
+                                    <NotesPanel/>
+
                                 </DialogPanel>
                             </TransitionChild>
                         </div>
